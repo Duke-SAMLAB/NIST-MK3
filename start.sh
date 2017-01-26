4ad60c622c180c73ddaf89dae17ce1854588623a
Anil <ag403@duke.edu>
2017-01-26 13:25:10 +0000

    get shell of service running

--- start.sh
+++ start.sh
@@ -5,12 +5,13 @@
 sudo ifconfig wlan0 10.0.0.1
 
 # make recordings directory if it doesn't exist
-if [ ! -d /home/pi/recordings]; then
+if [ ! -d /home/pi/recordings ]; then
 	mkdir /home/pi/recordings
 fi
 
 record_function() {
-	while [ -f /home/pi/recordings/recordings.continue]
+	touch /home/pi/recordings/recordings.continue
+	while [ -f /home/pi/recordings/recordings.continue ]
 	do
 		echo "We are recording" >> /home/pi/recordings/recordings.log
 		sleep 10
