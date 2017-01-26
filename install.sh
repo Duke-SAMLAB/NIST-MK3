50dbde960f3d044e925efe3fdf22a7bb7361afc8
Anil <ag403@duke.edu>
2017-01-26 14:48:32 +0000

    add dhcpd setup

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
+sudo apt-get install isc-dhcp-server
+
+cd /home/pi/micarray-pi
+echo "Configuring DHCPD service to interface with array"
+sudo cp dhcpd.confg /etc/dhcp/dhcpd.conf
+sudo cp interfaces /etc/network/interfaces
+sudo cp isc-dhcp-server /etc/default/isc-dhcp-server
+sudo service isc-dhcp-server restart
+
 echo "Adding recording service as a daemon service"
 
 sudo cp recording.service /lib/systemd/system/
-sudo cp ~/micarray-pi/start.sh /home/pi/
-sudo cp ~/micarray-pi/stop.sh /home/pi/
-sudo chmod +x ~/start.sh
-sudo chmod +x ~/stop.sh
+sudo cp start.sh /home/pi/
+sudo cp stop.sh /home/pi/
+sudo chmod +x /home/pi/start.sh
+sudo chmod +x /home/pi/stop.sh
 sudo systemctl daemon-reload
 sudo systemctl enable recording.service
