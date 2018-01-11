a3548d5950cbca77a4b3cf1ed2943f20dcc2619a
Anil <ag403@duke.edu>
2018-01-11 10:27:50 -0500

    use samlab wifi

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
-sudo apt-get install isc-dhcp-server git curl
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install isc-dhcp-server vim curl
 
 curl -O https://www.nist.gov/sites/default/files/documents/itl/iad/mig/smartspace/mk3.zip
 unzip mk3.zip
 sudo echo "export PATH=$PATH:/home/pi/mk3/mk3cap" >> /home/pi/.bashrc
-sudo source /home/pi/.bashrc
+sudo bash /home/pi/.bashrc
 
 cd /home/pi/micarray-pi
 echo "Configuring DHCPD service to interface with array"
-sudo cp dhcpd.confg /etc/dhcp/dhcpd.conf
+sudo cp dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo cp isc-dhcp-server /etc/default/isc-dhcp-server
+sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo service isc-dhcp-server restart
 
 echo "Adding recording service as a daemon service"
