fd4c963e9ff663c14f642a8ef0a8b387169a8f12
Anil Ganti <ag403@duke.edu>
2024-03-13 17:17:20 -0400

    update install script for miniconda

--- install.sh
+++ install.sh
@@ -1,17 +1,28 @@
 sudo apt-get update
+
+echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
+echo "export LANG=en_US.UTF-8" >> ~/.bashrc
+echo "export LANGUAGE=en_US.UTF-8" >> ~/.bashrc
+source ~/.bashrc
+
 sudo apt-get upgrade
 sudo apt-get install isc-dhcp-server vim curl
+
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
+/bin/bash Miniconda3-latest-Linux-armv7l.sh
+~/miniconda3/bin/conda init bash
+pip install jupyter
 
 curl -O https://www.nist.gov/sites/default/files/documents/itl/iad/mig/smartspace/mk3.zip
 unzip mk3.zip
 sudo echo "export PATH=$PATH:/home/pi/mk3/mk3cap" >> /home/pi/.bashrc
 sudo bash /home/pi/.bashrc
 
-cd /home/pi/micarray-pi
 echo "Configuring DHCPD service to interface with array"
 sudo cp dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo cp isc-dhcp-server /etc/default/isc-dhcp-server
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp jupyter.service /usr/lib/systemd/system/jupyter.service
 sudo /etc/init.d/networking reload
 sudo systemctl daemon-reload
 sudo /etc/init.d/networking restart
@@ -20,7 +31,6 @@ sudo service isc-dhcp-server restart
 echo "Adding recording service as a daemon service"
 
 #sudo cp recording.service /lib/systemd/system/
-sudo chmod +x /home/pi/micarray-pi/start.sh
-sudo chmod +x /home/pi/micarray-pi/stop.sh
-#sudo systemctl daemon-reload
+sudo systemctl daemon-reload
+sudo systemctl enable jupyter
 #sudo systemctl enable recording.service
